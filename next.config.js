1fdbb847e771caa28eda87fa10da067a5716b957
David Gaynor <runnedrun@gmail.com>
2022-12-13 23:56:09 +0100

    got rid of the directly override for next dist

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const CircularDependencyPlugin = require("circular-dependency-plugin")
 const prodHost = "https://send.hylitepeople.com"
 
 const config = {
-  distDir: "/tmp/.next",
   reactStrictMode: false,
   // swcMinify: true,
   // typescript: { tsconfigPath: "tsconfig.next.json" },
