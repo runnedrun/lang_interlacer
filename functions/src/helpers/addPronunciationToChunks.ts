1b17085950fd164466e47b96c76597082f4ab62a
Patrick Coleman <patrickscoleman@gmail.com>
2022-12-21 13:58:29 -0500

    Add imports and generic logic for JP pronunciation
    Use https://www.npmjs.com/package/kuroshiro which requires async/await
    Demo https://kuroshiro.org/#demo
    
    Struggling with correctly handling Promises, so just put in the logic
    with console logs. Then included the necessary kuroshiro functionality
    in the comments.

--- functions/src/helpers/addPronunciationToChunks.ts
+++ functions/src/helpers/addPronunciationToChunks.ts
@@ -1,7 +1,10 @@
 import { Language, Sentence } from "@/data/types/RawParagraph"
 import { objKeys } from "@/helpers/objKeys"
 import { Chunk } from "@/views/doc/ChunkDisplay"
-import pinyin from "pinyin"
+import pinyin from "pinyin" // Chinese pronunciation
+import Kuroshiro from "kuroshiro" // Japanese pronunciation
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji" // Japanese pronunciation
+const kuroshiro = new Kuroshiro()
 
 const isChinese = (text: string) => {
   const re = /[\u4e00-\u9fa5]/
@@ -40,6 +43,15 @@ const getPronunciationForSentences = (
 
         const text = pronunciationResults.filter(Boolean).flat().join(" ")
         return { sentenceIndex: sentence.sentenceIndex, text } as Sentence
+      } else if (lang === Language.Japanese) {
+        // const text = await kuroshiro.convert(sentence.text, {
+        //   to: "hiragana",
+        //   mode: "normal",
+        // })
+
+        // return { sentenceIndex: sentence.sentenceIndex, text } as Sentence
+        console.log("Got Japanese pronunciation for the sentence")
+        return null
       } else {
         return null
       }
@@ -53,7 +65,12 @@ export const addPronunciationToChunks = (chunks: Chunk[]) => {
   const lang1Language = getLanguageForSentences(chunks[0].lang1)
   const lang2Language = getLanguageForSentences(chunks[0].lang2)
 
-  const chunksWithPronunciations = chunks.map((chunk) => {
+  if (lang1Language === Language.Japanese) {
+    // await kuroshiro.init(new KuromojiAnalyzer())
+    console.log("The text is Japanese!")
+  }
+
+  const chunksWithPronunciations = chunks.map(async (chunk) => {
     chunk.lang1Pronunciation = getPronunciationForSentences(
       lang1Language,
       chunk.lang1
