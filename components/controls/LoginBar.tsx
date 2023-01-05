eb8a8e2135faf2dc34d5d4d295c4deab1f41dc83
Patrick Coleman <patrickscoleman@gmail.com>
2023-01-05 15:10:11 -0800

    add buttons

--- components/controls/LoginBar.tsx
+++ components/controls/LoginBar.tsx
@@ -1,3 +1,23 @@
-export default function LoginBar(loggedIn: boolean): JSX.Element {
-  return <div className="text-5xl mt-5 text-center">Language Interlacer</div>
+import { Button } from "@mui/material"
+import Link from "next/link"
+
+export default function LoginBar({
+  loggedIn,
+}: {
+  readonly loggedIn?: boolean
+}): JSX.Element {
+  return loggedIn ? (
+    <div className="w-full text-right my-2">
+      <Button className="mx-2">Log Out</Button>
+    </div>
+  ) : (
+    <div className="w-full text-right my-2">
+      <Button component={Link} className="mx-2">
+        Sign In
+      </Button>
+      <Button component={Link} className="mx-2" variant="contained">
+        Sign Up
+      </Button>
+    </div>
+  )
 }
